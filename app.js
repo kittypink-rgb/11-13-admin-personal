166af641ebc074921522b44ad7e1dc8d14121f0a
julian malfitano <jmalfitanom830@inemkennedy.edu.co>
2026-05-06 14:03:08 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const boton = document.querySelector('#btn-ingresar')
 const inputUsuario = document.querySelector('#usuario')
 const parrafoMensaje = document.querySelector('#mensaje')
+const inputContraseña = document.querySelector('#contraseña')
 
 boton.addEventListener('click', function() {
   const nombre = inputUsuario.value
+  const contraseña = inputContraseña.value
 
   if (nombre === '') {
     parrafoMensaje.textContent = 'Por favor ingresá tu nombre.'
+  } else if (contraseña === '') {
+    parrafoMensaje.textContent = 'Por favor ingresá tu contraseña.'
   } else {
     parrafoMensaje.textContent = `Bienvenido, ${nombre}`
   }
+ 
 })
+
